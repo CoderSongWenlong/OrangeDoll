6c42e2c7546d0adc85988a77fad39ad027445347
宋文龙 <wenlong.song@asmiao.com>
2018-06-22 15:44:26 +0800

    初始化项目

---
+++ 6_22Test/6_22Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  6_22Test
+//
+//  Created by 宋文龙 on 2018/6/22.
+//  Copyright © 2018年 Orange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
